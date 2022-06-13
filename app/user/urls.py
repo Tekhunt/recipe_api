931c59e933e32037351b7f4e00c4a341b9d2da3c
tekhunt <chizzydmec@gmail.com>
2022-06-13 09:51:52 +0100

    Feat: create user api with token authentication features

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import CreateUserView
+from .views import CreateUserView, ManageUserView, CreateTokenView
 
 app_name = 'user'
 
 
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
+    path('token/', CreateTokenView.as_view(), name='token'),
+    path('me/', ManageUserView.as_view(), name='me')
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -4,10 +4,23 @@ User API views
 """
 
 from yaml import serialize
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from .serializers import UserSerializer
+from .serializers import AuthTokenSerializer, UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
+    renderer_class = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
